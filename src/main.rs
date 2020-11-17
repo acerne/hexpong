29d16db9ad7ef77b071ef15b2eefbe30a918da46
acerne <a.cerne89@gmail.com>
2020-11-17 08:48:30 +0100

    Made hexagon bar movements alternating

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use ggez::*;
 use ggez::event::{KeyCode, KeyMods};
 use ggez::{event, graphics, Context, GameResult};
 
-const HEXAGON_SIZE: f32 = 200.0;
+const HEXAGON_SIZE: f32 = 300.0;
 const SCREEN_SIZE: (f32, f32) = (800.0, 600.0);
 const ORIGIN: (f32, f32) = (SCREEN_SIZE.0 / 2.0, SCREEN_SIZE.1 / 2.0);
 const BAR_SIZE: (f32, f32) = (50.0, 5.0);
@@ -26,17 +26,15 @@ struct Bar {
     pos: f32,
     w: f32,
     h: f32,
-    r: f32,
     phi: f32,
 }
 
 impl Bar {
-    pub fn new(r: f32, phi: f32) -> Self {
+    pub fn new(phi: f32) -> Self {
         Bar {
             pos: 0.5,
             w: BAR_SIZE.0,
             h: BAR_SIZE.1,
-            r: r,
             phi: phi,
         }
     }
@@ -44,11 +42,12 @@ impl Bar {
         Ok(())
     }
     fn draw(&self, ctx: &mut Context) -> GameResult {
-        // let xc = ORIGIN.0/ 4.0
-        //     + self.pos * HEXAGON_SIZE
-        //     + HEXAGON_SIZE * (self.phi + std::f32::consts::PI / 2.0).cos();
-        // let yc = ORIGIN.1 + HEXAGON_SIZE * (self.phi + std::f32::consts::PI / 2.0).sin();
-        let xc = ORIGIN.0 + -HEXAGON_SIZE / 2.0 + self.pos * HEXAGON_SIZE;
+        let xc = ORIGIN.0
+            + if (self.phi % (2.0 * std::f32::consts::PI / 3.0)) != 0.0 {
+                -HEXAGON_SIZE / 2.0 + self.pos * HEXAGON_SIZE - self.w / 2.0
+            } else {
+                HEXAGON_SIZE / 2.0 - self.pos * HEXAGON_SIZE - self.w / 2.0
+            };
         let yc = ORIGIN.1 + 3.0f32.sqrt() / 2.0 * HEXAGON_SIZE;
 
         let rect = graphics::Rect::new(xc, yc, self.w, self.h);
@@ -76,12 +75,6 @@ impl Bar {
     }
 }
 
-// fn polarToCarthesian(r: f32, phi: f32) -> mint::Point2<f32> {
-//     let x = r * phi.cos();
-//     let y = r * phi.sin();
-//     return mint::Point2 { x, y };
-// }
-
 struct GameState {
     bars: Vec<Bar>,
     barpos: f32,
@@ -92,10 +85,7 @@ impl GameState {
     pub fn new() -> Self {
         let mut bars = Vec::new();
         for ang in 0..6 {
-            bars.push(Bar::new(
-                HEXAGON_SIZE,
-                ang as f32 * std::f32::consts::PI / 3.0,
-            ));
+            bars.push(Bar::new(ang as f32 * std::f32::consts::PI / 3.0));
         }
         GameState {
             bars: bars,
@@ -108,15 +98,15 @@ impl GameState {
 impl event::EventHandler for GameState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         if self.input.left {
-            self.barpos -= 0.05;
+            self.barpos -= 0.03;
         } else if self.input.right {
-            self.barpos += 0.05;
+            self.barpos += 0.03;
         }
-        if self.barpos < 0.0 {
-            self.barpos = 0.0;
+        if self.barpos < (0.0 + BAR_SIZE.0 / HEXAGON_SIZE / 2.0) {
+            self.barpos = 0.0 + BAR_SIZE.0 / HEXAGON_SIZE / 2.0;
         }
-        if self.barpos > (1.0 - BAR_SIZE.0 / HEXAGON_SIZE) {
-            self.barpos = 1.0 - BAR_SIZE.0 / HEXAGON_SIZE;
+        if self.barpos > (1.0 - BAR_SIZE.0 / HEXAGON_SIZE / 2.0) {
+            self.barpos = 1.0 - BAR_SIZE.0 / HEXAGON_SIZE / 2.0;
         }
         for bar in self.bars.iter_mut() {
             bar.pos = self.barpos;
