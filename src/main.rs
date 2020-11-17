0006c798dc8bd74c5c976c520f745f136599a5d2
acerne <a.cerne89@gmail.com>
2020-11-17 11:40:26 +0100

    Added hexagonal grid

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use ggez::*;
 use ggez::event::{KeyCode, KeyMods};
 use ggez::{event, graphics, Context, GameResult};
 
+use std::collections::HashMap;
+
 const HEXAGON_SIZE: f32 = 300.0;
 const SCREEN_SIZE: (f32, f32) = (800.0, 600.0);
 const ORIGIN: (f32, f32) = (SCREEN_SIZE.0 / 2.0, SCREEN_SIZE.1 / 2.0);
@@ -66,7 +68,71 @@ impl Hexagon {
         )?;
         Ok(())
     }
+    fn draw_trace(&self, ctx: &mut Context) -> GameResult {
+        let vertices = self.get_vertices();
+        let trace = graphics::Mesh::new_polygon(
+            ctx,
+            graphics::DrawMode::stroke(3.0),
+            &vertices,
+            [0.8, 0.8, 0.8, 0.6].into(),
+        )?;
+        graphics::draw(
+            ctx,
+            &trace,
+            ggez::graphics::DrawParam::from((ggez::mint::Point2 { x: 0.0, y: 0.0 },)),
+        )?;
+        Ok(())
+    }
 }
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct GridIndex {
+    q: i32,
+    r: i32,
+    s: i32,
+}
+
+impl GridIndex {
+    fn to_pixel(&self, tile_radius: f32) -> mint::Point2<f32> {
+        let x = ORIGIN.0
+            + (self.q as f32 * 3.0f32.sqrt() + self.r as f32 * (3.0f32.sqrt() / 2.0)) * tile_radius;
+        let y = ORIGIN.1 + (3.0 / 2.0 * self.r as f32) * tile_radius;
+        mint::Point2 { x: x, y: y }
+    }
+}
+
+struct HexagonalGrid {
+    tiles: HashMap<GridIndex, Hexagon>,
+}
+
+impl HexagonalGrid {
+    pub fn new(grid_size: u16, tile_radius: f32) -> Self {
+        let grid_radius = ((grid_size + 1) / 2) as i32;
+        let mut tiles = HashMap::new();
+        for q in (-grid_radius + 1)..grid_radius {
+            for r in std::cmp::max(-grid_radius + 1, -q - grid_radius + 1)
+                ..=std::cmp::min(grid_radius - 1, -q + grid_radius - 1)
+            {
+                println!("{0}, {1}", q, r);
+                let s = -q - r;
+                let index = GridIndex { q: q, r: r, s: s };
+                let point = index.to_pixel(tile_radius);
+                tiles.insert(index, Hexagon::new(point.x, point.y, tile_radius));
+            }
+        }
+        HexagonalGrid { tiles: tiles }
+    }
+    fn draw(&self, ctx: &mut Context) -> GameResult {
+        for (_, hexagon) in &self.tiles {
+            hexagon.draw(ctx)?;
+        }
+        for (_, hexagon) in &self.tiles {
+            hexagon.draw_trace(ctx)?;
+        }
+        Ok(())
+    }
+}
+
 // !HEXMATH
 
 struct Bar {
@@ -124,7 +190,7 @@ impl Bar {
 
 struct GameState {
     bars: Vec<Bar>,
-    blocks: Vec<Hexagon>,
+    blocks: HexagonalGrid,
     barpos: f32,
     input: InputState,
 }
@@ -135,13 +201,9 @@ impl GameState {
         for ang in 0..6 {
             bars.push(Bar::new(ang as f32 * std::f32::consts::PI / 3.0));
         }
-        let mut blocks = Vec::new();
-        for dist in 0..6 {
-            blocks.push(Hexagon::new(ORIGIN.0 + dist as f32 * 40.0, ORIGIN.1, 20.0));
-        }
         GameState {
             bars: bars,
-            blocks: blocks,
+            blocks: HexagonalGrid::new(9, 20.0), // TODO as parameter
             barpos: 0.5,
             input: InputState::default(),
         }
@@ -151,7 +213,7 @@ impl GameState {
 impl event::EventHandler for GameState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         if self.input.left {
-            self.barpos -= 0.03;
+            self.barpos -= 0.03; // TODO as parameter + autoscale
         } else if self.input.right {
             self.barpos += 0.03;
         }
@@ -168,9 +230,7 @@ impl event::EventHandler for GameState {
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.0, 0.2, 0.3, 1.0].into());
-        for block in self.blocks.iter() {
-            block.draw(ctx)?;
-        }
+        self.blocks.draw(ctx)?;
         for bar in self.bars.iter() {
             bar.draw(ctx)?;
         }
